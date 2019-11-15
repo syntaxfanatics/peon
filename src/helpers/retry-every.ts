f8abfe8516f56e9e7bd290b92e6350195ac423c9
Nick Kelly <nick.kelly@ciasia.com.au>
2019-11-15 11:37:06 +1100

    add logging options to retry

--- src/helpers/retry-every.ts
+++ src/helpers/retry-every.ts
@@ -10,24 +10,24 @@ declare const console: { error(...args: any[]): void }
  *
  * @param options
  */
-export function retryEvery(options: { millisBetween: number; maxAttempts: number }) {
-  const { millisBetween, maxAttempts } = options;
+export function retryEvery(options: { millisBetween: number; maxAttempts: number; log?: ('attempts' | 'errors')[] }) {
+  const { millisBetween, maxAttempts, log } = options;
 
   return function takeAsyncFn<F extends AnyFunc>(tryMe: F) {
     let attempts = 0;
 
     return async function trigger<A extends Parameters<F>>(
       ...args: A
-    ): Promise<F extends (...args: A) => Promise<infer PR>
-        ? PR
-        : F extends(...args: A) => infer R
-          ? R : never> {
+    ): Promise<F extends (...args: A) => infer R ? R : never> {
       // eslint-disable-next-line no-constant-condition
       while(true) {
         try { return await tryMe(...args)  }
-        catch(error) { if (attempts >= maxAttempts) throw error }
+        catch(error) {
+          if (attempts >= maxAttempts) throw error
+          if (log?.includes('errors')) console?.error?.(error);
+        }
         attempts += 1;
-        console?.error?.(`Async Function "${tryMe.name || 'anonymous function'}" failed... Retrying (${attempts} of ${maxAttempts})`);
+        if (log?.includes('attempts')) console?.error?.(`Async Function "${tryMe.name || 'anonymous function'}" failed... Retrying (${attempts} of ${maxAttempts})`);
         await new Promise((res) => setTimeout(res, millisBetween));
       }
     }
