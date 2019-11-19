2e6b9eac6f20032f8caeb0411d19d9dc1ca40ffa
Nick Kelly <nick.kelly@ciasia.com.au>
2019-11-19 12:26:45 +1100

    make optional

--- src/helpers/helper-types.ts
+++ src/helpers/helper-types.ts
@@ -199,3 +199,9 @@ export type ThenArg<T> = T extends Promise<infer U> ? U : T;
  * Super type of any function
  */
 export type AnyFunc = (...args: any[]) => any;
+
+/**
+ * @description
+ * Make optional the chosen keys of an object
+ */
+export type MakeOptional<T, U extends keyof T> = Omit<T, U> & Partial<Pick<T, U>>
